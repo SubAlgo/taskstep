4c60fb7cd1bd2741723417bb430174c2863b2120
korn <kornchaval@gmail.com>
2017-11-24 21:13:00 +0700

    Show appoint in Create ics page

--- app/Http/Controllers/appointController.php
+++ app/Http/Controllers/appointController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\appoint;
+use DB;
 
 class appointController extends Controller
 {
     public function getappoint() {
         $appoint = new appoint;
         
-        $data = $appoint->get();
-        //DB::table('task')->join('appoint', 'task.id' , '=' , 'appoint.task_id')->select('task.title', 'appoint.date','appoint.id')->get();
+        //$data = $appoint->get();
+        $data = DB::table('task')->join('appoint', 'task.id' , '=' , 'appoint.task_id')->orderBy('appoint.id')->select('task.title', 'appoint.date','appoint.id')->get();
 
         return response($data);
     }
--- resources/views/createics.blade.php
+++ resources/views/createics.blade.php
@@ -3,7 +3,15 @@
 
 <div class="ui stacked segment">
     <label for="">กำหนดการ</label>
+    
     <form id="theList" action="">
+        <table id="appointTable" border="1">
+            <tr>
+                <th>Check</th>
+                <th>เดือน/วัน/ปี ชั่วโมง:นาที</th>
+                <th>Task</th>
+            </tr>
+        </table>
         
     </form>
 </div>
@@ -21,16 +29,35 @@ $(window).load(function(){
         type: "GET",
         url: "getappoint",
         dataType: 'json',
-        success: function (data) {
+        success: function(data) {
            
-            var i = 0;
+            //var i = 0;
+           
             var myData = JSON.stringify(data)
+            var dataParse = JSON.parse(myData)
+            /*
             var dataParse = JSON.parse(myData)
             vData = dataParse
             dataParse.forEach(function (element) {
-                $('#theList').append("<input type='checkbox' name='appointlist' id='appointlist' value=" + element.id + ">" + element.date + '<br>')
+                $('#theList').append("<input type='checkbox' name='appointlist' id='appointlist' value=" + element.id + ">" + element.date +  '<br>')
             });
+            */
+            
+
+            dataParse.forEach(function (element) {
+                $('#appointTable').append("<tr>\
+                                            <td align='center'><input type='checkbox' name='da' value=" + element.id + "></td>\
+                                            <td>" + element.date + "</td>\
+                                            <td>" + element.title + "</td>\
+                                        </tr>")
+            })
             $('#theList').append("<input type='submit' value='Submit'>")
+            
+            
+
+
+
+            //$('#theList').append("<input type='submit' value='Submit'>")
             
             
             
